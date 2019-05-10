70dcdc3d0fd73b271dcaf01d32064c3ae8d09075
Gopalasivam <gopalasivam.palaniappan@eplus.com>
2019-05-10 01:02:14 -0700

    Added additional Ruby RVM and Rails packages on the script.

--- install-website.sh
+++ install-website.sh
@@ -2,3 +2,18 @@
 sudo apt-get update 
 sudo apt-get install -y python-opencv 
 sudo apt install -y nginx 
+sudo apt-get install -y unicorn
+sudo apt-get install -y curl gnupg build-essential
+sudo apt-get install -y python-pip python-dev
+sudo apt-get install -y software-properties-common
+
+sudo apt-add-repository -y ppa:rael-gc/rvm
+sudo apt update
+sudo apt install -y rvm
+echo '[[ -s "/usr/share/rvm/scripts/rvm" ]] && . "/usr/share/rvm/scripts/rvm" # Load RVM function' >> ~/.bashrc
+echo '[[ -s "/usr/share/rvm/scripts/rvm" ]] && . "/usr/share/rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
+source ~/.bash_profile
+rvm fix-permissions
+rvmsudo rvm install 2.2.2
+rvmsudo rvm install 2.5.3
+sudo gem install rails -v 4.1.0
