a4b66b2dfab640518e95622fa3ae8ccad97663ff
Gopalasivam <gopalasivam.palaniappan@eplus.com>
2019-05-10 02:04:30 -0700

    Added RVM start script

--- install-website.sh
+++ install-website.sh
@@ -10,10 +10,11 @@ sudo apt-get install -y software-properties-common
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt update
 sudo apt install -y rvm
+source /etc/profile.d/rvm.sh
 echo '[[ -s "/usr/share/rvm/scripts/rvm" ]] && . "/usr/share/rvm/scripts/rvm" # Load RVM function' >> ~/.bashrc
 echo '[[ -s "/usr/share/rvm/scripts/rvm" ]] && . "/usr/share/rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
 source ~/.bash_profile
-rvm fix-permissions
+rvmsudo rvm fix-permissions
 rvmsudo rvm install 2.2.2
 rvmsudo rvm install 2.5.3
 sudo gem install rails -v 4.1.0
